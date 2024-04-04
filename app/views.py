2694dee028e65f53ab2e65097b9999cc2e58e237
unknown <matheus.melo@centralit.com.br>
2024-04-04 16:58:38 -0300

    Criando tela de detalhes dos funcionários e fazendo links entre telas.

--- app/views.py
+++ app/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from app.models import Employee
 
-# Create your views here.
+def employee_detail(request, pk):
+    # try:
+    #     employee = Employee.objects.get(pk=pk)
+    #     print(employee)
+    # except:
+    #     raise Http404
+        employee = get_object_or_404(Employee, pk=pk)
+        context = {
+            "employee": employee,
+        }
+        # print(employee)
+        return render(request, 'employee_detail.html', context)
